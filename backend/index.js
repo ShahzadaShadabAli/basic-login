ee1b03e320cab6cdf77c04d1521393359f1de5dd
ShadabShahzada <126250733+ShahzadaShadabAli@users.noreply.github.com>
2024-07-18 22:46:18 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 // }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://basic-login-sigma.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://basic-login-sigma.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
